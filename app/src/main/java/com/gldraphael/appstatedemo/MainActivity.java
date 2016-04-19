aaa4d3538990390894dcf1ea95e2091f1361b228
Galdin Raphael <gldraphael@gmail.com>
2016-04-19 20:04:29 +0530

    Create UI

--- app/src/main/java/com/gldraphael/appstatedemo/MainActivity.java
+++ app/src/main/java/com/gldraphael/appstatedemo/MainActivity.java
@@ -2,12 +2,24 @@ package com.gldraphael.appstatedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtDescription;
+    private CheckBox cbxMilk;
+    private CheckBox cbxLaundry;
+    private CheckBox cbxBed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtDescription = (EditText) findViewById(R.id.txtDescription);
+        cbxMilk = (CheckBox) findViewById(R.id.cbxMilk);
+        cbxLaundry = (CheckBox) findViewById(R.id.cbxLaundry);
+        cbxBed = (CheckBox) findViewById(R.id.cbxBed);
     }
 }
